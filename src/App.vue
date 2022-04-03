b760b3ebd69150c80dd0b44b3d443b3929f42bb0
Jose Duarte <josegduarte96@gmail.com>
2022-04-03 16:59:11 -0300

    Primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container-principal" class="container-fluid position-relative h-100">
+    <div style="z-index: 9999; width: 100%; right: 0px;" class="position-fixed bg-white">
+      <nav id="menu" class="row align-items-center p-2 mx-0 mx-sm-2 mx-md-3 mx-lg-5">
+        <div id="logo" class="col-5 col-sm-3 col-md-2">
+          <img src="./assets/logo.svg" alt="logo-easybank">
+        </div>
+        <div class="col-sm-5 col-md-8 d-none d-sm-none d-md-block">
+            <div class="container-fluid d-flex justify-content-center text-info">
+              <a href="" class="navbar-brand">Home</a>
+              <a href="" class="navbar-brand">About</a>
+              <a href="" class="navbar-brand">Contact</a>
+              <a href="" class="navbar-brand">Blog</a>
+              <a href="" class="navbar-brand">Careers</a>
+            </div>
+        </div>
+        <div class="col-7 col-sm-7 col-md-2">
+          <img @click="isModalOpen" v-if="!modalOpen" data-bs-toggle="modal" data-bs-target="#exampleModal" class="float-end d-block d-sm-block d-md-none me-4" src="./assets/icon-hamburger.svg" alt="mobile-menu-icon">
+          <img @click="isModalOpen" data-bs-dismiss="modal" data-bs-target="#exampleModal" v-else src="./assets/icon-close.svg" class="float-end d-block d-sm-block d-md-none me-4" alt="">
+          <button class="d-none d-sm-none d-md-block btn btn-light bg-gradient float-end rounded-pill text-white">
+                Request Invite
+          </button>
+        </div>
+        <!-- Menu Mobile -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <ul class="list-group d-flex align-items-center p-3">
+                <li class="list-group-item border-0">Home</li>
+                <li class="list-group-item border-0">About</li>
+                <li class="list-group-item border-0">Contact</li>
+                <li class="list-group-item border-0">Blog</li>
+                <li class="list-group-item border-0">Careers</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    </nav>
+    </div>
+   <Body/> 
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from '@vue/reactivity'
+import Body from './layouts/Body.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Body },
+  setup() {
+    const modalOpen = ref(false)
+    return {
+      isModalOpen: () => modalOpen.value = !modalOpen.value,
+      modalOpen
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#menu{
+  height: 85px;
 }
+  $navbar-brand-font-size: 0.9rem;
+  $gradient: linear-gradient(to left, #2bb7da, #00ccd5, #00dcb2, #3ae875);
+  $btn-padding-x: 10px;
+  $font-family-sans-serif: 'Public Sans', sans-serif;
+  $primary:       hsl(233, 26%, 24%);
+  $secondary:     hsl(136, 65%, 51%);
+  $info:          hsl(233, 8%, 62%);
+  $success:       hsl(192, 70%, 51%);
+  $light:         hsl(220, 16%, 96%);
+  $danger:        #F9F9F9;
+  // $modal-backdrop-bg:  linear-gradient(to-left, black, white);
+  $container-max-widths: (
+  sm: 540px,
+  md: 720px,
+  lg: 960px,
+  xl: 1140px,
+  xxl: 1440px
+);
+
+.modal{
+  top: 90px !important;
+}
+.container-fluid{
+  height: 45px;
+}
+.container-fluid .navbar-brand:hover{
+  border-bottom: 3px solid $secondary;
+}
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
 </style>
---
+++ src/layouts/Body.vue
@@ -0,0 +1,225 @@
+<template>
+<div class="position-relative">
+    <div class="bg-mockup d-none d-md-block">
+        <div class="bg2-desktop"></div>
+    </div>
+    <section class="container-fluid position-relative bg-light p-0" style="height: 100vh; overflow: hidden">
+      <div class="bg-desktop d-none d-md-block">
+      </div>
+      <div class="container-fluid position-relative h-100 row mx-0 px-0 align-items-center">
+          <div class="bg-mobile d-block d-md-none">
+              <div class="bg2-mobile"></div>
+          </div>
+          <div class="col-12 col-sm-8 col-md-7 col-lg-5 m-lg-5 m-md-3 m-sm-1">
+              <h1 class="text-center text-md-start text-lg-start">Next generation digital banking</h1>
+              <p class="fs-5 text-info text-center text-md-start text-lg-start">Take your financial life online. Your Easybank account will be a one-stop-shop 
+                 for spending, saving, budgeting, investing, and much more.
+              </p>
+              <div class="col text-center text-md-start text-lg-start">
+                <button class="btn btn-light bg-gradient rounded-pill text-white">
+                    Request Invite
+                </button>
+              </div>
+          </div>
+      </div>
+    </section>    
+    <section class="container-fluid bg-danger p-0 pt-5" style="height: 100%;">
+      <div class="container w-100 mx-1 mx-sm-2 mx-md-3 mx-lg-5 p-1 px-sm-2 px-md-3 px-lg-5">
+          <div class="row">
+              <div class="col-12 col-sm-10 col-md-7 col-lg-5 p-1">
+                <h1 class="text-center text-md-start text-lg-start">Why choose Easybank?</h1>
+                <p class="fs-4 text-info text-center text-md-start text-lg-start">We leverage Open Banking to turn your bank account into your financial hub. Control 
+                  your finances like never before.
+                </p>
+              </div>
+          </div>
+          <div class="row mt-5">
+              <div class="col-12 col-sm-3 col-md-3 col-lg-3 d-flex flex-column align-items-center align-items-md-start align-items-lg-start">
+                  <img style="height: 100px; width: 100px" class="p-2" src="../assets/icon-online.svg" alt="">
+                  <h3 class="p-2">Online Banking</h3>
+                  <p class="p-2 text-info">
+                    Our modern web and mobile applications allow you to keep track of your finances 
+                    wherever you are in the world.
+                  </p>
+              </div>
+              <div class="col-12 col-sm-3 col-md-3 col-lg-3 d-flex flex-column align-items-center align-items-md-start align-items-lg-start">
+                  <img style="height: 100px; width: 100px" class="p-2" src="../assets/icon-budgeting.svg" alt="">
+                  <h3 class="p-2">Simple Budgeting</h3>
+                  <p class="p-2 text-info">
+                    See exactly where your money goes each month. Receive notifications when you’re 
+                    close to hitting your limits.
+                  </p>
+              </div>
+              <div class="col-12 col-sm-3 col-md-3 col-lg-3 d-flex flex-column align-items-center align-items-md-start align-items-lg-start">
+                  <img style="height: 100px; width: 100px" class="p-2" src="../assets/icon-onboarding.svg" alt="">
+                  <h3 class="p-2">Fast Onboarding</h3>
+                  <p class="p-2 text-info">
+                    We don’t do branches. Open your account in minutes online and start taking control 
+                    of your finances right away.
+                  </p>
+              </div>
+              <div class="col-12 col-sm-3 col-md-3 col-lg-3 d-flex flex-column align-items-center align-items-md-start align-items-lg-start">
+                  <img style="height: 100px; width: 100px" class="p-2" src="../assets/icon-api.svg" alt="">
+                  <h3 class="p-2">Open API</h3>
+                  <p class="p-2 text-info">
+                    Our modern web and mobile applications allow you to keep track of your finances 
+                    wherever you are in the world.
+                  </p>
+              </div>
+          </div>
+      </div>
+    </section>
+</div>
+    <section class="container-fluid bg-light px-0 py-5" style="height: 100%;">
+      <div class="container w-100 p-1 px-sm-2 px-md-3 px-lg-5 pt-5">
+          <div class="row">
+              <div class="col-12 col-sm-10 col-md-7 col-lg-5 p-1">
+                <h1 class="text-center text-md-start text-lg-start">Latest Articles</h1>
+              </div>
+          </div>
+          <div class="row mt-5">
+              <div class="my-3 my-md-0 my-lg-0 col-12 col-sm-3 col-md-3 col-lg-3">
+                  <div class="card">
+                    <img src="../assets/image-currency.jpg" class="card-img-top img-fluid" alt="img-confetti">
+                    <div class="card-body">
+                       <blockquote>
+                          <p class="fs-6 text-info my-3">
+                            By Claire Robinson
+                           </p>
+                           <h5>Receive money in any currency with no fees</h5>
+                           <p class="card-text text-info">
+                                The world is getting smaller and we’re becoming more mobile. So why should you be 
+                                forced to only receive money in a single …
+                            </p>
+                       </blockquote>
+                    </div>
+                </div>
+              </div>
+              <div class="my-3 my-md-0 my-lg-0 col-12 col-sm-3 col-md-3 col-lg-3">
+                  <div class="card">
+                    <img src="../assets/image-restaurant.jpg" class="card-img-top img-fluid" alt="img-confetti">
+                    <div class="card-body">
+                        <blockquote>
+                          <p class="fs-6 text-info my-3">
+                            By Wilson Hutton
+                           </p>
+                           <h5>Treat yourself without worrying about money</h5>
+                           <p class="card-text text-info">                                
+                                Our simple budgeting feature allows you to separate out your spending and set 
+                                realistic limits each month. That means you …
+                            </p>
+                       </blockquote>
+                    </div>
+                </div>
+              </div>
+              <div class="my-3 my-md-0 my-lg-0 col-12 col-sm-3 col-md-3 col-lg-3">
+                  <div class="card">
+                    <img src="../assets/image-plane.jpg" class="card-img-top img-fluid" alt="img-confetti">
+                    <div class="card-body">
+                        <blockquote>
+                          <p class="fs-6 text-info my-3">
+                            By Wilson Hutton
+                           </p>
+                           <h5>Take your Easybank card wherever you go</h5>
+                           <p class="card-text text-info">                                
+                                We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
+                                while you’re abroad. We’ll even show you …
+                            </p>
+                       </blockquote>
+                    </div>
+                </div>
+              </div>
+              <div class="my-3 my-md-0 my-lg-0 col-12 col-sm-3 col-md-3 col-lg-3">
+                <div class="card">
+                    <img src="../assets/image-confetti.jpg" class="card-img-top img-fluid" alt="img-confetti">
+                    <div class="card-body">
+                        <blockquote>
+                          <p class="fs-6 text-info my-3">
+                            By Claire Robinson
+                           </p>
+                           <h5>Our invite-only Beta accounts are now live!</h5>
+                           <p class="card-text text-info">                                
+                                After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
+                                It’s easy to request an invite through the site ...
+                            </p>
+                       </blockquote>
+                    </div>
+                </div>
+              </div>
+          </div>
+      </div>
+    </section>
+    <FooterVue/>
+</template>
+
+<script>
+import FooterVue from './Footer.vue'
+
+export default {
+components: {FooterVue}
+}
+</script>
+
+<style lang="scss">
+
+h5{
+    cursor: pointer;
+    &:hover{
+        color: hsl(192, 70%, 51%);
+    }
+}
+
+h1{
+    font-size: 38px !important;
+}
+.img-fluid{
+    height: 12rem !important;
+}
+.card-text{
+    font-size: 13px;
+}
+.bg-desktop{
+    width: 100%;
+    position: absolute;
+    left: 45%;
+    height: 100%;
+    background-size: auto;
+    background-repeat: no-repeat;
+    background-position: 0;
+    background-image: url('../assets/bg-intro-desktop.svg');
+}
+
+.bg-mobile{
+    background-image: url('../assets/bg-intro-mobile.svg');
+    height: 350px;
+    padding: 0;
+    background-size: 100%;
+    background-repeat: no-repeat;
+}
+.bg2-mobile{
+    background-image: url('../assets/image-mockups.png');
+    width: 100%;
+    background-size: 70%;
+    height: 100%;
+    z-index: 999;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+.bg-mockup{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    overflow: hidden;
+}
+.bg2-desktop{
+    background-image: url('../assets/image-mockups.png');
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    right: -60%;
+    top: -20%;
+    background-repeat: no-repeat;
+    background-position: 0;
+}
+</style>
